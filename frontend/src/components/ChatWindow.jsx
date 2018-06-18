2122e91a3a6436b9df0729aa27471ff4f2d94c2b
Daniel Szabo <dgszabo@gmail.com>
2018-06-18 14:52:06 -0700

    Frontend - minor chanes to component import

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MessagesTab from './MessagesTab';
 import SendMessageTab from '../containers/SendMessageTab';
 import './ChatWindow.css'
